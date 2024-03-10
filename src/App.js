845a25d6c87210bcdb91c9662fb6abd0b9c8bf78
emrhncbn <emrhncbn@gmail.com>
2024-03-10 22:31:57 +0300

    Form İnput Commit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function FormInput({label,inputPlaceholder,inputDescription}) {
         <input
           id='email'
           name='email'
-          type='email'
+          type={label.toLowerCase() === "password" ? "password" : "email"}
           placeholder={inputPlaceholder}
           className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2'
         />
@@ -33,9 +33,9 @@ export default function App() {
       inputPlaceholder='you@example.com'
       inputDescription='Bunu sadece spam için kullanacağız.' />
       <FormInput
-      label='Diğer Alan'
-      inputPlaceholder='Diğer değer'
-      inputDescription='Bu alan hakkında açıklama.' />
+      label='Password'
+      inputPlaceholder='Your password'
+      inputDescription='Mesaj Yok' />
     </form>
   )
 }
