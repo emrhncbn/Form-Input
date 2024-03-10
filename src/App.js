26fe64c9eb535e2a50ee82c02f60bffe461c37c1
emrhncbn <emrhncbn@gmail.com>
2024-03-10 22:36:21 +0300

    Form İnput Commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 // Statik değerleri alın ve prop'ları kullanarak dinamik hale getirin
 // Örneğin: label id, label text, input placeholder, input description
 function FormInput({label,inputPlaceholder,inputDescription}) {
+
+  const isPassword = label === 'Password'
+
   return (
     <div className='max-w-sm p-8'>
       <label
-        htmlFor='email'
+        htmlFor={label}
         className='block text-sm font-medium leading-6 text-gray-900'
       >
         {label}
       </label>
       <div className='mt-2'>
         <input
-          id='email'
-          name='email'
-          type={label.toLowerCase() === "password" ? "password" : "email"}
+          id={label}
+          name={label}
+          type={isPassword ? "password" : "email"}
           placeholder={inputPlaceholder}
           className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2'
         />
